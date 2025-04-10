03857c3a90089cfdea9f16c72a7bfc4823722a80
AlexAraldi <alexanderaraldi@gmail.com>
2025-04-10 15:57:43 -0300

    Implementado a criação de todos os módulos e arquivos

---
+++ ClubeDaLeitura.ConsoleApp/ModuloCaixas/TelaCaixa.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace ClubeDaLeitura.ConsoleApp.ModuloCaixas
+{
+// Requisitos Funcionais: 
+//  ● O sistema deve permitir cadastrar novas caixas 
+//  ● O sistema deve permitir editar caixas existentes 
+//  ● O sistema deve permitir excluir caixas
+//  ● O sistema deve permitir visualizar todas as caixas
+// Regras de Negócio: 
+//  ● Campos obrigatórios: 
+//    ○ Etiqueta(texto único, máximo 50 caracteres)
+//    ○ Cor(seleção de paleta ou hexadecimal)
+//    ○ Dias de empréstimo(número, padrão 7)
+//  ● Não pode haver etiquetas duplicadas 
+//  ● Não permitir excluir uma caixa caso tenha revistas vinculadas 
+//  ● Cada caixa define o prazo máximo para empréstimo de suas revistas
+
+
+    class TelaCaixa
+    {
+    }
+}
---
+++ ClubeDaLeitura.ConsoleApp/ModuloRevistas/TelaRevista.cs
@@ -0,0 +1,24 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ClubeDaLeitura.ConsoleApp.ModuloRevistas
+{
+//  Requisitos Funcionais:
+//      ● O sistema deve permitir cadastrar novas revistas
+//      ● O sistema deve permitir editar revistas existentes
+//      ● O sistema deve permitir excluir revistas
+//      ● O sistema deve permitir visualizar todas as revistas
+//      ● O sistema deve mostrar o status atual(disponível/emprestada/reservada)
+//  Regras de Negócio:
+//      ● Campos obrigatórios:
+//        ○ Título(2-100 caracteres)
+//        ○ Número da edição(número positivo)
+//        ○ Ano de publicação(data válida)
+//        ○ Caixa(seleção obrigatória)
+//      ● Não pode haver revistas com mesmo título e edição
+//      ● Status possíveis: Disponível / Emprestada / Reservada
+//      ● Ao cadastrar, o status padrão é "Disponível"
+    class TelaRevista
+    {
+    }
+}
